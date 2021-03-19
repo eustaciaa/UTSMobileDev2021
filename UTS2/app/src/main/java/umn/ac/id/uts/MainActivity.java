c6a5bf98438a688e5424b729e24ea672fde33931
eustaciaa <clara3@student.umn.ac.id>
2021-03-20 00:57:32 +0700

    added plain login and profile activity

--- UTS2/app/src/main/java/umn/ac/id/uts/MainActivity.java
+++ UTS2/app/src/main/java/umn/ac/id/uts/MainActivity.java
@@ -2,16 +2,40 @@ package umn.ac.id.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.login);
+        btnProfile = findViewById(R.id.profile);
+
+        btnProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentMenuA = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivityForResult(intentMenuA, 1);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentMenuA = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(intentMenuA, 1);
+            }
+        });
     }
 }
